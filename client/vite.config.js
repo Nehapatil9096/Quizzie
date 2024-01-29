d15f865e0e3c78ae4d3da85265cf821b4ffb1998
b30wulffz <nehapatil9096@gmail.com>
2024-01-30 00:08:21 +0530

    vite5

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  //base: '/Quizzie/', // Update this to your repository name
+  // Plugins
   plugins: [react()],
-})
+
+  // Build Configuration
+  build: {
+    outDir: 'build', // Specify the output directory here
+  },
+});
