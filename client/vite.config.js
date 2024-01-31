5ca757b9b5c570123f85f83465e1ac0d5f670924
b30wulffz <nehapatil9096@gmail.com>
2024-01-31 18:25:11 +0530

    versal1

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   // Plugins
   plugins: [react()],
-  base:"/Quizzie/",
-  
+  base: "/Quizzie/",
+  // Explicitly mark react-redux as external
+  build: {
+    rollupOptions: {
+      external: ['react-redux']
+    }
+  }
 });
