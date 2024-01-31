8f025b837824dda5e9eb96097f9dd5ad9e81e7c5
b30wulffz <nehapatil9096@gmail.com>
2024-01-31 21:44:23 +0530

    final2

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,11 +5,5 @@ export default defineConfig({
   // Plugins
   plugins: [react()],
   base: "/Quizzie/",
-  // Explicitly mark react-redux and @reduxjs/toolkit as external
-  build: {
-    rollupOptions: {
-      external: ['react-redux', '@reduxjs/toolkit', 'react-toastify']
-
-    }
-  }
+ 
 });
